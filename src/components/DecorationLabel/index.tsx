578f76ab6bce31bf46c0f5c1b44c221c1991714f
Streyter <35563213+Streyter@users.noreply.github.com>
2023-10-02 12:20:49 +0200

    Update index.tsx

--- src/components/DecorationLabel/index.tsx
+++ src/components/DecorationLabel/index.tsx
@@ -34,45 +34,42 @@ interface Props {
         style={{background: "#8DCFDE",
           borderRadius:"20px",
           width:"100%",
+          height:"50%",
+          position: "absolute",
+          zIndex: "-100",
+          top: "60%",
+          right: "1rem"
+
+        }}/>
+
+      </div>
+     ) :(
+      
+      <div
+        style={{
+          position: "relative",
+          zIndex: "100",
+          fontSize:"64px",
+          fontWeight: "400",
+          width: "max-content",
+          border: "solid red 1px"
+        }}
+      >
+       {/* <img src={img}/> */}
+
+        <div
+        style={{background: "#8DCFDE",
+          borderRadius:"20px",
+          width:"100%",
           height:"100%",
           position: "absolute",
           zIndex: "-100",
           top: "1rem",
           right: "1rem"
-          // height:"50%",
-          // top: "60%",
         }}/>
 
         </div>
-     ) :(
-      <img src={img}/>
-<div
-style={{
-  position: "relative",
-  zIndex: "100",
-  fontSize:"64px",
-  fontWeight: "400",
-  width: "max-content",
-  border: "solid red 1px"
-}}
->
-  {content}
-
-<div
-style={{background: "#8DCFDE",
-  borderRadius:"20px",
-  width:"100%",
-  height:"100%",
-  position: "absolute",
-  zIndex: "-100",
-  top: "1rem",
-  right: "1rem"
-  // height:"50%",
-  // top: "60%",
-}}/>
-
-</div>
-     )}
+      )}
     </>
   );
   
