924522e840286e931b9224606502b9fcd2f81130
Dawid Strzyż <35563213+Streyter@users.noreply.github.com>
2023-11-01 22:13:01 +0100

    Modal added

--- src/views/Profile/index.tsx
+++ src/views/Profile/index.tsx
@@ -17,10 +17,17 @@ import styles from "./styles.module.scss";
 import { format } from "date-fns";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Toast, ToastStatus } from "../../components/Toast";
 import { LocalizationProvider } from "@mui/x-date-pickers";
-
+import { Postcard } from "../../components/Postcard";
+import { useGetPaginatedPostardQuery } from "../../services/postcard.service";
+import BoxM from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import * as React from 'react';
+import { UserData } from "../../models/user";
 export const Profile = () => {
   const user = useTypedSelector((state) => state.auth.user);
 
@@ -35,8 +42,18 @@ export const Profile = () => {
       },
     }
   );
-  
+
   const [updateUserData] = useUpdateUserDataMutation();
+
+  const [user123, setUser123] = useState<UserData|undefined>(undefined);
+  
+  useEffect(() => {
+    console.log('xd');
+    // if (!userData) {
+    //   return;
+    // }
+    setUser123(userData);
+  }, [user123,userData,refetch]); 
 
   const [isEdit, setIsEdit] = useState<boolean>(false);
 
@@ -46,17 +63,33 @@ export const Profile = () => {
     "Something went wrong"
   );
 
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+
   const initialValue = {
     id: user?.id ?? "0",
-    firstName: userData?.firstName,
-    lastName: userData?.lastName,
-    nickName: userData?.nickName,
-    country: userData?.country,
-    city: userData?.city,
-    birthDate: userData?.birthDate,
-    avatarBase64: userData?.avatarBase64,
-    backgroundBase64: userData?.backgroundBase64,
-    description: userData?.description,
+    firstName: user123?.firstName,
+    lastName: user123?.lastName,
+    nickName: user123?.nickName,
+    country: user123?.country,
+    city: user123?.city,
+    birthDate: user123?.birthDate,
+    avatarBase64: user123?.avatarBase64,
+    backgroundBase64: user123?.backgroundBase64,
+    description: user123?.description,
   };
 
   const userSchema = Yup.object().shape({
@@ -73,9 +106,11 @@ export const Profile = () => {
   const { values, errors, touched, setFieldValue, submitForm } = useFormik({
     initialValues: initialValue,
     validationSchema: userSchema,
+
     onSubmit: async (values) => {
       await updateUserData(values)
         .then((response: any) => {
+          console.log(response);
           if (response.error) {
             setToastStatus("error");
             setToastErrorMessage(
@@ -87,6 +122,7 @@ export const Profile = () => {
           refetch();
         })
         .catch((e: { message: any }) => {
+          console.log(e);
           setToastStatus("error");
           setToastErrorMessage(e.message ?? "Something went wrong");
         });
@@ -112,7 +148,7 @@ export const Profile = () => {
 
       <Box className={styles.profile} sx={{ background: "background.paper" }}>
         <img
-          src={`data:image/jpeg;base64,${userData?.backgroundBase64}`}
+          src={`data:image/jpeg;base64,${user123?.backgroundBase64}`}
           alt="background"
           className={styles.background_image}
         />
@@ -120,8 +156,65 @@ export const Profile = () => {
           <Avatar
             className={styles.avatar_image}
             alt="Avatar"
-            src={`data:image/jpeg;base64,${userData?.avatarBase64}`}
+            src={`data:image/jpeg;base64,${user123?.avatarBase64}`}
           />
+          <Button onClick={handleOpen}>Edit Profile</Button>
+          <span>user123{user123?.firstName}</span>
+                  <span>values{values.firstName}</span>
+          <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <BoxM sx={style}>
+                
+                  <div className={styles.fullName}>
+
+                  <TextField
+                    variant="outlined"
+                    value={values.firstName}
+                    error={Boolean(errors.firstName && touched.firstName)}
+                    onChange={(e) => setFieldValue("firstName", e.target.value)}
+                  />
+                  <TextField
+                    variant="outlined"
+                    value={values.lastName}
+                    error={Boolean(errors.lastName && touched.lastName)}
+                    onChange={(e) => setFieldValue("lastName", e.target.value)}
+                  />
+                  <TextField
+                  variant="outlined"
+                  value={values.country}
+                  error={Boolean(errors.country && touched.country)}
+                  onChange={(e) => setFieldValue("country", e.target.value)}
+                />
+                <TextField
+                  variant="outlined"
+                  value={values.city}
+                  error={Boolean(errors.city && touched.city)}
+                  onChange={(e) => setFieldValue("city", e.target.value)}
+                />
+                  <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <DatePicker
+                    value={new Date(values.birthDate ?? 0) ?? new Date()}
+                    onChange={(e) => e && setFieldValue("birthDate", e)}
+                    format="dd.MM.yyyy"
+                  />
+                </LocalizationProvider>
+                
+                </div>
+                <span className={styles.update} onClick={() =>{
+                  handleUpdateUser();
+                  setOpen(false);
+                  refetch();
+                }}>
+                  <CheckIcon />
+                  Update profile
+                </span>
+                </BoxM>
+              </Modal>
+
           <div>
             {!isEdit ? (
               <span
@@ -152,78 +245,43 @@ export const Profile = () => {
         <div className={styles.profile_info}>
           <div className={styles.info}>
             <span className={styles.info_name}>
-              {!isEdit ? (
+              
                 <>
-                  {userData?.firstName ?? "-"} {userData?.lastName ?? "-"}
+                  {user123?.firstName ?? "-"} {user123?.lastName ?? "-"}
                 </>
-              ) : (
-                <div className={styles.fullName}>
-                  <TextField
-                    variant="outlined"
-                    value={values.firstName}
-                    error={Boolean(errors.firstName && touched.firstName)}
-                    onChange={(e) => setFieldValue("firstName", e.target.value)}
-                  />
-                  <TextField
-                    variant="outlined"
-                    value={values.lastName}
-                    error={Boolean(errors.lastName && touched.lastName)}
-                    onChange={(e) => setFieldValue("lastName", e.target.value)}
-                  />
-                </div>
-              )}
+              
             </span>
           </div>
           <div className={styles.info}>
             <span className={styles.info_name}>Nickname:</span>
             <span className={styles.info_value}>
-              {userData?.nickName ?? "-"}
+              {user123?.nickName ?? "-"}
             </span>
           </div>
           <div className={styles.bio}>
             <div className={styles.country}>
               <FlagIcon />
-              {!isEdit ? (
-                <span>{userData?.country ?? "-"}</span>
-              ) : (
-                <TextField
-                  variant="outlined"
-                  value={values.country}
-                  error={Boolean(errors.country && touched.country)}
-                  onChange={(e) => setFieldValue("country", e.target.value)}
-                />
-              )}
+              
+                <span>{user123?.country ?? "-"}</span>
+              
             </div>
             <div className={styles.city}>
               <LocationCityIcon />
-              {!isEdit ? (
-                <span>{userData?.city ?? "-"}</span>
-              ) : (
-                <TextField
-                  variant="outlined"
-                  value={values.city}
-                  error={Boolean(errors.city && touched.city)}
-                  onChange={(e) => setFieldValue("city", e.target.value)}
-                />
-              )}
+              
+                <span>{user123?.city ?? "-"}</span>
+              
             </div>
             <div className={styles.birth_date}>
               <CakeIcon />
-              {!isEdit ? (
+              
                 <span>
-                  {userData?.birthDate
-                    ? format(new Date(userData.birthDate), "dd.MM.yyyy")
+                  {user123?.birthDate
+                    ? format(new Date(user123.birthDate), "dd.MM.yyyy")
                     : "-"}
                 </span>
-              ) : (
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                  <DatePicker
-                    value={new Date(values.birthDate ?? 0) ?? new Date()}
-                    onChange={(e) => e && setFieldValue("birthDate", e)}
-                    format="dd.MM.yyyy"
-                  />
-                </LocalizationProvider>
-              )}
+              
+                
+              
             </div>
           </div>
         </div>
@@ -232,18 +290,18 @@ export const Profile = () => {
           <div className={styles.info}>
             <span className={styles.info_name}>Postcards received</span>
             <span className={styles.info_value}>
-              {userData?.postcardsReceived ?? "-"}
+              {user123?.postcardsReceived ?? "-"}
             </span>
           </div>
           <div className={styles.info}>
             <span className={styles.info_name}>Postcards sent</span>
             <span className={styles.info_value}>
-              {userData?.postcardsSent ?? "-"}
+              {user123?.postcardsSent ?? "-"}
             </span>
           </div>
           <div className={styles.info}>
             <span className={styles.info_name}>Total score</span>
-            <span className={styles.info_value}>{userData?.score ?? "-"}</span>
+            <span className={styles.info_value}>{user123?.score ?? "-"}</span>
           </div>
         </div>
       </Box>
